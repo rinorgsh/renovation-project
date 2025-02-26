e3581d980dc9253a3e0e62755984444142263195
54718 <54718@etu.he2b.be>
2025-02-26 12:32:42 +0100

    add ressource

--- resources/js/Components/InputError.vue
+++ resources/js/Components/InputError.vue
@@ -1,11 +1,15 @@
 <script setup>
 defineProps({
-    message: String,
+    message: {
+        type: String,
+    },
 });
 </script>
 
 <template>
-    <div v-show="message" class="text-sm text-red-600">
-        {{ message }}
+    <div v-show="message">
+        <p class="text-sm text-red-600">
+            {{ message }}
+        </p>
     </div>
 </template>
